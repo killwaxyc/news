75ba46e1223ff8f1c799605e0e60ffd50d8da3ba
killwa <superkillwa@163.com>
2018-10-12 22:08:03 +0800

    注册的实现

--- info/modules/passport/views.py
+++ info/modules/passport/views.py
@@ -1,8 +1,8 @@
 import random
 import re
 
-from flask import current_app, jsonify, make_response, request
-from info import constants, redis_store
+from flask import current_app, jsonify, make_response, request, session
+from info import constants, redis_store, db
 from info.lib.yuntongxun.sms import CCP
 from info.models import User
 from . import passport_blu
@@ -114,3 +114,67 @@ def send_sms():
 
     # 7. 返回发送成功的响应
     return jsonify(errno=RET.OK, errmsg="发送成功")
+
+
+@passport_blu.route('/register', methods=['POST'])
+def register():
+    """
+        1. 获取参数和判断是否有值
+        2. 从redis中获取指定手机号对应的短信验证码的
+        3. 校验验证码
+        4. 初始化 user 模型，并设置数据并添加到数据库
+        5. 保存当前用户的状态
+        6. 返回注册的结果
+        :return:
+        """
+    json_data = request.json
+    mobile = json_data.get('mobile')
+    sms_code = json_data.get('smscode')
+    password = json_data.get('password')
+
+    if not all([mobile, sms_code, password]):
+        # 参数不全
+        return jsonify(errno=RET.PARAMERR, errmsg="参数不全")
+
+    try:
+        real_smscode = redis_store.get('SMS_'+mobile)
+    except Exception as e:
+        current_app.logger.error(e)
+        # 获取本地验证码失败
+        return jsonify(errno=RET.DBERR, errmsg="获取本地验证码失败")
+
+    if not real_smscode:
+        # 短信验证码过期
+        return jsonify(errno=RET.NODATA, errmsg="短信验证码过期")
+
+    if sms_code != real_smscode.decode():
+        return jsonify(errno=RET.DATAERR, errmsg="短信验证码错误")
+    # 删除短信验证码
+    try:
+        redis_store.delete('SMS_'+mobile)
+    except Exception as e:
+        current_app.logger.error(e)
+
+    user = User()
+    user.nick_name = mobile
+    user.mobile = mobile
+    # 对密码进行处理
+    user.password = password
+
+    # 提交到数据库
+    try:
+        db.session.add(user)
+        db.session.commit()
+    except Exception as e:
+        db.session.rollback()
+        current_app.logger.error(e)
+        # 数据保存错误
+        return jsonify(errno=RET.DATAERR, errmsg="数据保存错误")
+
+    # 保存登录状态
+    session['user_id'] = user.id
+    session['nick_name'] = user.nick_name
+    session['mobile'] = mobile
+
+    # 6. 返回注册结果
+    return jsonify(errno=RET.OK, errmsg="OK")
