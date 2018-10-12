07ba16906434e3469cf941a239dcc01c207e33a7
killwa <superkillwa@163.com>
2018-10-12 22:41:02 +0800

    用户登录前后的首页的不同展示

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,11 +1,22 @@
+from info.models import User
 from info.modules.index import index_blu
 from manage import app
-from flask import render_template, current_app
+from flask import render_template, current_app, session
 
 
 @index_blu.route('/')
 def index():
-    return render_template('news/index.html')
+    # 获取到当前登录用户的id
+    user_id = session.get("user_id")
+    # 通过id获取用户信息
+    user = None
+    if user_id:
+        try:
+            user = User.query.get(user_id)
+        except Exception as e:
+            current_app.logger.error(e)
+
+    return render_template('news/index.html', data={"user_info": user.to_dict() if user else None})
 
 
 @index_blu.route('/favicon.ico')
